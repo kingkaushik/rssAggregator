93e1b42167a09ef12edad2db135fa16579aeb958
pranaytanniru <pranaytanniru1132@gmail.com>
2019-02-27 12:15:18 +0530

    handled the cases where all the fields are not coming from the feed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const rp = require('request-promise');
-const url = 'https://9to5google.com/feed/';
+// const url = 'https://9to5google.com/feed/';
+const url = 'https://timesofindia.indiatimes.com/rssfeedstopstories.cms'
 const xml2js = require ('xml2js')
 const response=[]
 var httpServer=http.createServer(unified)
@@ -15,11 +16,11 @@ function unified(req,respo){
         let item={}
         items.map((a,i)=>{
           let item={}
-          item.title=items[i].title[0];
-          item.link=items[i].link[0];
-          item.date=items[i].pubDate[0];
-          item.category=items[i].category[0];
-          item.description=items[i].description[0];
+          item.title=items[i].title?items[i].title[0]:'No title';
+          item.link=items[i].link?items[i].link[0]:'Link not available';
+          item.date=items[i].pubDate?items[i].pubDate[0]:'Published date not available';
+          item.category=items[i].category?items[i].category[0]:'Category not available';
+          item.description=items[i].description?items[i].description[0]:'Description not available';
           response.push(item)
         })
         var payloadString=JSON.stringify(response)
